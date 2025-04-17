0c12cabefeb245edaf2aa26a7ebd9b04467ae515
MalkuSR <m.s.malik3105@gmail.com>
2025-04-17 23:43:08 +0500

    updated

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -24,7 +24,7 @@ import { Home, Search, Explore, Reels, Message, Notification, Create, Profile, T
 // Icons components
 import Logo from "../../../assets/svg/NavbarIcons/Logo.jsx";
 import RoutesIcons from "../../../assets/svg/NavbarIcons/RoutesIcons.jsx";
-import { UtilityIcons24 } from "../../../../../client/src/assets/svg/NavbarIcons/utilityIcons.jsx";
+import { UtilityIcons24 } from "../../../assets/svg/NavbarIcons/utilityIcons.jsx";
 // all exports
 export { Notification, UtilityIcons24, iscreatemodal, Input, SearchInput, MobileNav, NotifiModal, CreateModal, Home, Explore, Reels, Message, Create, Profile, Threads, List, Logo, useWidth, usemodal, Search, MoreModal, ApearenceModal, SeachModal, Modal, flex, Navlogo, Hamberger, darktheme2, RoutesIcons };
 
