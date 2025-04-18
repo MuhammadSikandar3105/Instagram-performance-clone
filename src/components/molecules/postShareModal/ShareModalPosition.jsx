d45c015737da55775f351d048dbeeaa9fa463529
MalkuSR <m.s.malik3105@gmail.com>
2025-04-19 02:37:33 +0500

    share modal almost completed

--- src/components/molecules/postShareModal/ShareModalPosition.jsx
+++ src/components/molecules/postShareModal/ShareModalPosition.jsx
@@ -14,7 +14,7 @@ const ShareModalPosition = ({ children, setShare }) => {
       className="bg-[#000000ae] w-full h-full fixed top-1/2 -translate-y-1/2"
     >
       <div
-        className={`max-w-[548px] max-h-[65vh] p-4 overflow-hidden w-full h-full top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 rounded-[16px] searchmodalshadow modalbg text-secondary absolute 
+        className={`max-w-[548px] max-h-[65vh] overflow-hidden w-full h-full top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 rounded-[16px] searchmodalshadow modalbg text-secondary absolute 
    ${darkTheme && "darkmode"}`}
         onClick={(e) => e.stopPropagation()}
       >
--- src/components/molecules/postShareModal/index.js
+++ src/components/molecules/postShareModal/index.js
@@ -1,4 +1,5 @@
 import ShareModalPosition from "./ShareModalPosition";
 import HeaderModal from "../../atoms/HeaderModal";
+import RoutesIcons from "../../../assets/svg/NavbarIcons/RoutesIcons";
 
-export { ShareModalPosition, HeaderModal }
+export { ShareModalPosition, HeaderModal, RoutesIcons }
